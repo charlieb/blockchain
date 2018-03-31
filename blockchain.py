7a87adb4c6ca60027ab8faa57c6b1830e74e57bf
Charlie Burrows <charlie.burrows@gmail.com>
2018-03-31 00:54:42 -0400

    Refactor and random tx generator

--- blockchain.py
+++ blockchain.py
@@ -5,6 +5,7 @@ from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat
 from cryptography.hazmat.primitives.hashes import Hash, SHA224
 from cryptography.exceptions import InvalidSignature
 from base64 import b64encode, b64decode
+import random
 
 def new_key():
     return ec.generate_private_key(ec.SECP256K1, default_backend())
@@ -38,15 +39,22 @@ def verify(pubkey, signature, message):
 #---------------------------------------
 
 def get_tx(bc, txid):
-    return bc[txid]
+    return bc['txes'][txid]
 
 def add_tx(bc, tx):
-    tx['txid'] = len(bc)
-    bc.append(tx)
+    if not verify_tx(bc, tx):
+        return None
+    tx['txid'] = len(bc['blocks'])
+    bc['blocks'].append(tx)
+
+    bc['txes'][tx['txid']] = tx
+    bc['utxos'] -= set((inp['txid'], inp['output']) for inp in tx['inputs'])
+    bc['utxos'] |= set((tx['txid'], i) for i,_ in enumerate(tx['outputs']))
+
     return tx['txid']
 
 def mk_tx(inputs, pubkeys, outputs):
-    return {'inputs': inputs, 'pubkeys': pubkeys, 'outputs': outputs}
+    return {'txid': None, 'inputs': inputs, 'pubkeys': pubkeys, 'outputs': outputs, 'signatures': []}
 
 def to_sign(tx):
     return b''.join(str(x).encode() for x in tx['inputs'] + tx['pubkeys'] + tx['outputs'])
@@ -68,77 +76,93 @@ def verify_sig(bc, tx):
             print('Invalid Tx - pubkey to address mismatch\n%s'%tx)
             return False
 
-
     # Verify that each signature is correct for the tx body and the public key
     for sig, pubkey in zip(tx['signatures'], tx['pubkeys']):
         message = to_sign(tx)
         if not verify(txt_pub(pubkey), sig, message):
             print('Invalid Tx - bad signature\n%s'%tx)
             return False
-
-def verify_chain(bc):
-    utxos = set()
-    for tx in bc:
-        print(tx)
-        # Validate and sum inputs
-        in_amt = 0
-        for inp in tx['inputs']:
-            if (inp['txid'], inp['output']) not in utxos:
-                print('Invalid Tx - double spend of %s:%s:\n%s'%(inp['txid'], inp['output'], tx))
-                return False
-            in_amt += bc[inp['txid']]['outputs'][inp['output']]['amount']
-
-        # Sum outputs
-        out_amt = sum(out['amount'] for out in tx['outputs'])
-
-        # must be enough in inputs or no inputs at all for a special tx
-        if in_amt < out_amt and tx['inputs'] != []:
-            print('Invalid Tx - output > input:\n%s'%tx)
-            return False
-        
-        # Finally update utxo database
-        utxos -= set((inp['txid'], inp['output']) for inp in tx['inputs'])
-        utxos |= set((tx['txid'], i) for i,_ in enumerate(tx['outputs']))
     return True
 
+def verify_tx(bc, tx):
+    # Validate and sum inputs
+    in_amt = 0
+    spent = []
+    for inp in tx['inputs']:
+        utxo = (inp['txid'], inp['output'])
+        if utxo not in bc['utxos'] or utxo in spent:
+            print('Invalid Tx - double spend of %s:%s:\n%s'%(inp['txid'], inp['output'], tx))
+            return False
+        in_amt += bc['txes'][inp['txid']]['outputs'][inp['output']]['amount']
+        spent.append(utxo)
+
+    # Sum outputs
+    out_amt = sum(out['amount'] for out in tx['outputs'])
+
+    # must be enough in inputs or no inputs at all for a special tx
+    if in_amt < out_amt and tx['inputs'] != []:
+        print('Invalid Tx - output > input:\n%s'%tx)
+        return False
+
+    if not verify_sig(bc, tx):
+        return False
+
+    return True
+
+def try_add_tx(bc, tx):
+    txid = add_tx(bc, tx)
+    if txid is None:
+        print('Add tx failed')
+        return False
+    else:
+        print('Added tx %s'%txid)
+        return True
+
+def gen_tx(bc, addr_keys):
+    keys = [new_key() for _ in range(random.randint(1,5))]
+    utxos = random.sample(tuple(bc['utxos']), random.randint(1,min(len(bc['utxos']), 5)))
+
+    outputs = [get_tx(bc, utxo[0])['outputs'][utxo[1]] for utxo in utxos]
+    utxo_keys = [addr_keys[out['address']] for out in outputs]
+
+    total = sum(out['amount'] for out in outputs)
+    dividers = sorted(random.sample(range(1, total), len(keys) - 1))
+    amts = [a - b for a, b in zip(dividers + [total], [0] + dividers)]
+    
+    tx = mk_tx([mk_input(tx, out) for tx,out in utxos],
+               [pub_txt(key.public_key()) for key in utxo_keys],
+               [mk_output(address(k.public_key()), amt) for k, amt in zip(keys, amts)])
+    sign_tx(tx, utxo_keys)
+    
+    if not try_add_tx(bc, tx):
+        print('gen_tx Failed')
+        return False
+
+    for k in keys: addr_keys[address(k.public_key())] = k
+
+def sum_utxos(bc):
+    return sum(get_tx(bc, utxo[0])['outputs'][utxo[1]]['amount'] for utxo in bc['utxos'])
+
 def test():
-    bc = []
+    bc = {'blocks': [], 'txes': {}, 'utxos': set()}
+
     a,b,c = new_key(), new_key(), new_key()
-
-    tx = mk_tx([], [], [mk_output(address(a.public_key()), 100)])
-    add_tx(bc, tx)
-
-    tx = mk_tx([mk_input(0, 0)], 
-               [pub_txt(a.public_key())],
-               [mk_output(address(b.public_key()), 75), mk_output(address(c.public_key()), 25)])
-    sign_tx(tx, [a])
-    add_tx(bc, tx)
-    verify_sig(bc, tx)
-
-    tx = mk_tx([mk_input(1, 0)],
-               [pub_txt(b.public_key())],
-               [mk_output(address(b.public_key()), 35), mk_output(address(c.public_key()), 40)])
-    sign_tx(tx, [b])
-    add_tx(bc, tx)
-
-    print(bc)
-    print(verify_chain(bc))
-
-    print('---------------------------')
-
-    key = new_key()
-    print(prv_txt(key))
-    print(pub_txt(key.public_key()))
-    print(address(key.public_key()))
-    print(pub_txt(key.public_key()) == pub_txt(txt_pub(pub_txt(key.public_key()))))
-
-    message = b'This is a test'
-    sig = sign(key, message)
-    print(sig)
-    print(verify(key.public_key(), sig, message))
+    addr_keys = { 
+            address(a.public_key()): a,
+            address(b.public_key()): b,
+            address(c.public_key()): c,
+            }
 
 
-    print('---------------------------')
+    tx = mk_tx([], [], [mk_output(address(a.public_key()), 1000000)])
+    try_add_tx(bc, tx)
+    print(sum_utxos(bc))
+
+    for _ in range(500):
+        gen_tx(bc, addr_keys)
+        print(sum_utxos(bc))
+
+
 
 if __name__ == '__main__':
     test()
